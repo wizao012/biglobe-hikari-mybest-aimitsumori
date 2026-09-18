889430602e5c3e1e915d6e62b5230ad9f2c21626
wizao012 <ao_marketing@012grp.co.jp>
2026-09-18 14:12:05 +0900

    Add files via upload

---
+++ assets/lp-config.js
@@ -0,0 +1,8 @@
+window.LP_CONFIG = Object.freeze({
+  "productionHosts": [
+    "biglobe-hikari-mybest-aimitsumori.pages.dev"
+  ],
+  "webhookUrl": "https://hooks.zapier.com/hooks/catch/12525485/44eksus/",
+  "gtmId": "GTM-T88QDFX3",
+  "ga4Id": "G-JSSG78FK7D"
+});
